497a79ad8383b8cb84870fffaaaa83125b1f1d1f
mcc <andrew.mcclure@gmail.com>
2012-01-04 00:39:59 -0800

    Mingw compatibility 5-- fix some compile errors, still symbol issues in libPolycore

--- CMake/PolycodeIncludes.cmake
+++ CMake/PolycodeIncludes.cmake
@@ -1,9 +1,9 @@
 # If the following short ugly mess is not present, failures can occur with -G "Unix Makefiles".
 # FIXME: cmake should be finding ALL of the following things by way of CMAKE_PREFIX_PATH
 IF (MINGW)
-SET(OGG_LIBRARY ${POLYCODE_RELEASE_DIR}/Framework/Core/Dependencies/lib)
+SET(OGG_LIBRARY libogg)
 SET(OGG_INCLUDE_DIR ${POLYCODE_RELEASE_DIR}/Framework/Core/Dependencies/include)
-SET(PNG_LIBRARY ${POLYCODE_RELEASE_DIR}/Framework/Core/Dependencies/lib)
+SET(PNG_LIBRARY png)
 SET(PNG_PNG_INCLUDE_DIR ${POLYCODE_RELEASE_DIR}/Framework/Core/Dependencies/include)
 ENDIF()
 
