377dafb5ace88868262e40c6146f2c61101380a4
mcc <andi.m.mcclure@gmail.com>
2012-05-05 12:48:24 -0700

    Zlib 1.2.7 (patch from Niall Moody)

--- CMake/ExternalPNG.cmake
+++ CMake/ExternalPNG.cmake
@@ -17,8 +17,8 @@ EXTERNALPROJECT_ADD(zlib
     PREFIX ${libpng_PREFIX}
 
     DOWNLOAD_DIR ${POLYCODE_DEPS_DOWNLOAD_DIR}
-    URL http://zlib.net/zlib-1.2.6.tar.gz
-    URL_MD5 618e944d7c7cd6521551e30b32322f4a
+    URL http://zlib.net/zlib-1.2.7.tar.gz
+    URL_MD5 60df6a37c56e7c1366cca812414f7b85
 
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PolycodeDependencies_SOURCE_DIR}/../CMake/zlib.cmake <SOURCE_DIR>/CMakeLists.txt && ${CMAKE_COMMAND} -E remove <SOURCE_DIR>/zconf.h
 
